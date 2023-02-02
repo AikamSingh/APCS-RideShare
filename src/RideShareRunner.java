86626412afe1d9a09b3177447d7defa9b75fd2d8
Aikam Singh <aikamsingh07@gmail.com>
2023-02-02 09:07:01 -0800

    finished station method(for now). will begin work on car class

--- src/RideShareRunner.java
+++ src/RideShareRunner.java
@@ -7,15 +7,23 @@
 
 public class RideShareRunner {
     public static void main(String[] args) {
-        //creates a passenger
+        //creates passengers
         Passenger p1 = new Passenger(1, 2);
+        Passenger p2 = new Passenger(1, 2);
         //creates a new station
-        Station s = new Station();
+        Station s1 = new Station();
+        Station s2 = new Station();
         //prints out information about the passenger
         System.out.println("p1 = " + p1.toString());
-        s.addPassenger(p1);
+        //adds passengers
+        s1.addPassenger(p1);
+        s1.addPassenger(p2);
         //prints out number of passengers in the station
-        System.out.println(s.getNumPassengers());
+        System.out.println("Number of passengers in Station 1: " + s1.getNumPassengers()); //expects 2
+        //prints out information about the station #1
+        System.out.println(s1.toString());
+        //prints out infor of station #2
+        System.out.println(s2.toString());
 
 
     }
--- src/Station.java
+++ src/Station.java
@@ -2,16 +2,21 @@ import java.util.ArrayList;
 
 /**
  * write description
- *
  * @author Aikam Singh
  * @version 1/31/23
  */
 
 public class Station {
     ArrayList<Passenger> passengers;
+    private boolean hasPassenger;
+    private int stationNum;
+    private static int lastStationNum = 0;
 
     public Station(){
         passengers = new ArrayList<>();
+        hasPassenger = false;
+        lastStationNum++;
+        stationNum = lastStationNum;
     }
 
     public int getNumPassengers(){
@@ -20,5 +25,30 @@ public class Station {
 
     public void addPassenger(Passenger passenger){
         passengers.add(passenger);
+    }
+
+    public boolean hasPassenger(){
+        if(passengers.size() > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String toString(){
+        String result = "Station #" + stationNum + " with size "+ passengers.size() + ":\n";
+
+        for(int i = 0; i < passengers.size(); i++){
+            if(i < passengers.size() - 1){
+                result += "passenger #" + i + " with destination " + passengers.get(i).getDestination() + ", \n";
+            }
+            else{
+                result += "passenger #" + (i + 1) + " with destination " + passengers.get(i).getDestination();
+            }
+
+        }
+
+        return result.trim();
     }
 }
